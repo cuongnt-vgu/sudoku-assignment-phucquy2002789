0d0a72982a8e02d8bce6654faf96b5ad4af5b62e
phucquy2002789 <154372807+phucquy2002789@users.noreply.github.com>
2023-12-31 23:51:34 +0700

    Update hidden_singles.h

--- src/hidden_singles.h
+++ src/hidden_singles.h
@@ -9,11 +9,10 @@ typedef struct HiddenSingle_impl
 } HiddenSingle;
 
 // find hidden single values (the value that is unique among all the candidates for a row, a collumn, or a box)
-int find_hidden_single_values(Cell **p_cells, int *hidden_single_values);
+void find_hidden_single_values(Cell *p_cell, int hidden_single_values[9]);
 
 // find hidden single cells in a row, in a collumn or in a box
-void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles,
-                        int *p_counter);
+int find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles, int *p_counter);
 
 // find hidden single cells in a board, return the number of hidden single cells
 int hidden_singles(SudokuBoard *p_board);
