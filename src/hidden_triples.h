4b2a06bdcd45e1fa309f4f55d3d6dfe78789ed24
phucquy2002789 <154372807+phucquy2002789@users.noreply.github.com>
2023-12-31 23:51:57 +0700

    Update hidden_triples.h

--- src/hidden_triples.h
+++ src/hidden_triples.h
@@ -2,4 +2,17 @@
 
 #include "sudoku.h"
 
+typedef struct HiddenTriple_impl {
+    Cell *p_cells[3];
+    int value1;
+    int value2;
+    int value3;
+} HiddenTriple;
+
 int hidden_triples(SudokuBoard *p_board);
+
+bool is_hidden_triple(Cell **p_cells, int value1, int value2, int value3);
+bool check_in_cell(Cell *p_cell, int value1, int value2, int value3);
+
+int find_hidden_triple_values(Cell **p_cells, int *hidden_triple_values);
+void find_hidden_triple(Cell **p_cells, HiddenTriple *p_hidden_triple, int *p_counter);
