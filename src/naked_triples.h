f19c7a2eb9facf64b2e810900dd051f26fa9ea4d
phucquy2002789 <154372807+phucquy2002789@users.noreply.github.com>
2023-12-31 23:52:39 +0700

    Update naked_triples.h

--- src/naked_triples.h
+++ src/naked_triples.h
@@ -1,5 +1,21 @@
+// naked_triples.h
 #pragma once
 
 #include "sudoku.h"
 
+// Structure to represent a Naked Triple
+typedef struct NakedTriple_impl {
+    Cell *p_cell_1;
+    Cell *p_cell_2;
+    Cell *p_cell_3;
+    Cell **p_cells;
+    int value1;
+    int value2;
+    int value3;
+} NakedTriple;
+
+bool is_naked_triple(Cell **p_cells, int value1, int value2, int value3);
+void remove_candidates_except_naked_triple(Cell **p_cells, int value1, int value2, int value3);
+int find_naked_triple_values(Cell **p_cells, int *naked_triple_values);
+void find_naked_triple(Cell **p_cells, NakedTriple *p_naked_triple, int *p_counter);
 int naked_triples(SudokuBoard *p_board);
